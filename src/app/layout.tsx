790165879e9daf01c7159b3bf2b77cda7ebeb623
jigong2024 <choijming21@naver.com>
2024-09-26 11:07:00 +0900

    네이게이션바 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <header className="flex flex-row justify-evenly items-center">
+          <Link href="/">홈</Link>
+          <Link href="/champions">챔피언 목록</Link>
+          <Link href="/items">아이템 목록</Link>
+          <Link href="/rotation">챔피언 로테이션</Link>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
