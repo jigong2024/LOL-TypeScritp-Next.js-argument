4ba5bf64e319a194a04c6c720b4f55e275d7edc5
jigong2024 <choijming21@naver.com>
2024-09-26 11:35:22 +0900

    홈화면 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-items-start gap-10 mt-10">
+      <h1>리그 오브 레전드 정보 앱</h1>
+      <h3>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</h3>
+      <div className="flex flex-col">
+        <Link href="/champions">챔피언 목록</Link>
+        <Link href="/rotation">금주 로테이션</Link>
+        <Link href="/items">아이템 목록보기</Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
