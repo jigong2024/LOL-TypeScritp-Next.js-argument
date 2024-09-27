9bf9bbe1c3a4d5e22b1a05c2b1109a12c3f98f77
jigong2024 <choijming21@naver.com>
2024-09-27 19:33:32 +0900

    특정 챔피언 엔드포인트 서버 액션 작성

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,10 +1,10 @@
-import { Champion } from "@/types/Champion";
+"use server";
 
-export const getChampionList = async () => {
-  "use server";
+import { Champion, ChampionDetail } from "@/types/Champion";
 
+// 최신 버전 가져오는 함수
+const getVersion = async () => {
   try {
-    // 최신 버전 가져오기
     const versionResponse = await fetch(
       "https://ddragon.leagueoflegends.com/api/versions.json"
     );
@@ -14,33 +14,92 @@ export const getChampionList = async () => {
     }
 
     const versions: string[] = await versionResponse.json();
-    const latestVersion = versions[0];
+    return versions[0];
+  } catch (error) {
+    console.error("Error fetching version:", error);
+    throw error;
+  }
+};
+
+// 챔피언 목록 가져오는 함수
+export const getChampionList = async () => {
+  try {
+    // 최신 버전 가져오기
+    const version = await getVersion();
 
     // 버전에 맞춰 챔피언 목록 가져오기
     const championListResponse = await fetch(
-      `https://ddragon.leagueoflegends.com/cdn/${latestVersion}/data/ko_KR/champion.json`
+      `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
     );
 
     if (!championListResponse.ok) {
       throw new Error("Failed to fetch champion list data");
     }
 
-    const fullChampionListData: { data: Record<string, any> } =
+    const { data }: { data: Record<string, any> } =
       await championListResponse.json();
 
     // 필요한 챔피언 데이터만 추출
-    const championList: Champion[] = Object.values(
-      fullChampionListData.data
-    ).map((champion) => ({
+    const championList: Champion[] = Object.values(data).map((champion) => ({
       id: champion.id,
       name: champion.name,
       title: champion.title,
-      image: champion.image,
+      image: {
+        full: champion.image.full,
+      },
     }));
 
     return championList;
   } catch (error) {
     console.error("Error fetching champion list:", error);
+    throw error;
+  }
+};
+
+// 특정 챔피언 상세 정보 가져오기
+export const getChampionDetail = async (
+  id: string
+): Promise<ChampionDetail> => {
+  try {
+    // 최신 버전 가져오기
+    const version = await getVersion();
+
+    const championDetailResponse = await fetch(
+      `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/${id}.json`
+    );
+
+    if (!championDetailResponse.ok) {
+      throw new Error("Failed to fetch champion detail data");
+    }
+
+    const { data }: { data: Record<string, any> } =
+      await championDetailResponse.json();
+
+    const champion = data[id];
+
+    if (!champion) {
+      throw new Error(`Champion data for ${id} not found`);
+    }
+
+    const championDetail: ChampionDetail = {
+      id: champion.id,
+      name: champion.name,
+      title: champion.title,
+      image: {
+        full: champion.image.full,
+      },
+      blurb: champion.blurb,
+      info: {
+        attack: champion.info.attack,
+        defense: champion.info.defense,
+        magic: champion.info.magic,
+        difficulty: champion.info.difficulty,
+      },
+    };
+
+    return championDetail;
+  } catch (error) {
+    console.error("Error fetching champion detail:", error);
     throw error;
   }
 };
