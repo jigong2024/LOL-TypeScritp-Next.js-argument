f92314871d2135cbc3f4fe2c6da6403007cd0e49
jigong2024 <choijming21@naver.com>
2024-09-30 16:11:22 +0900

    상세정보페이지 동적 메타데이터 설정

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,8 +1,24 @@
 import { ChampionDetail } from "@/types/Champion";
 import { getChampionDetail, getVersion } from "@/utils/serverApi";
+import { Metadata } from "next";
 import Image from "next/image";
 
 export const revalidate = 86400; // 1일(86400초) 후 재검증(ISR)
+
+// 동적 메타 데이터
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const championId = params.id;
+  const championDetail = await getChampionDetail(championId);
+
+  return {
+    title: `${championDetail.name} - 리그 오브 레전드 챔피언 정보`,
+    description: championDetail.blurb,
+  };
+}
 
 async function ChampionDetailPage({ params }: { params: { id: string } }) {
   const championId = params.id;
