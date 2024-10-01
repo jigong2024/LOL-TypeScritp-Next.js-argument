2039b3b21bd39d1407455b974491e5b33f25e599
jigong2024 <choijming21@naver.com>
2024-10-01 12:27:10 +0900

    폰트적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Metadata } from "next";
+// import "../styles/globals.css";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import poroImage from "../public/img/poro.png";
+import rotationImage from "../public/img/rotation.png";
+import itemImage from "../public/img/item.png";
 
 // 정적 메타 데이터
 export const metadata: Metadata = {
@@ -14,9 +19,25 @@ const HomePage = () => {
       <h1>리그 오브 레전드 정보 앱</h1>
       <h3>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</h3>
       <div className="flex flex-col">
-        <Link href="/champions">챔피언 목록</Link>
-        <Link href="/rotation">금주 로테이션</Link>
-        <Link href="/items">아이템 목록보기</Link>
+        <Link href="/champions">
+          <Image src={poroImage} alt="챔피언 목록" width={500} height={300} />
+          <p>챔피언 목록</p>
+        </Link>
+
+        <Link href="/rotation">
+          <Image
+            src={rotationImage}
+            alt="금주 로테이션"
+            width={500}
+            height={300}
+          />
+          <p>금주 로테이션</p>
+        </Link>
+        <Link href="/items">
+          <Image src={itemImage} alt="아이템 목록" width={500} height={300} />
+
+          <p>아이템 목록보기</p>
+        </Link>
       </div>
     </div>
   );
