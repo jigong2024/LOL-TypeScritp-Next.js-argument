d2c59915866f11e4ca75fa7cde0bd44822935c3d
jigong2024 <choijming21@naver.com>
2024-10-01 17:16:18 +0900

    반응형 css

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -27,7 +27,7 @@ async function ChampionDetailPage({ params }: { params: { id: string } }) {
 
   return (
     <div className="w-full flex justify-center">
-      <div className="w-[80%] flex flex-col items-center gap-10 mt-8">
+      <div className="w-[80%] flex flex-col items-center gap-10 mt-8 mb-10 lg:mb-5">
         <div>
           <div className="text-[20px] font-bold text-red-500">
             {championDetail.name}
